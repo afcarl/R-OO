86c5c09c1dada34b119de47c8d2b753d7bfadd47
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2015-08-12 10:12:43 +0900

    Update set-complicated-method.R

--- src/set-complicated-method.R
+++ src/set-complicated-method.R
@@ -20,11 +20,11 @@ setClass("MyInteger", representation(value = "numeric"))
 setClass("MyChar", representation(value = "character"))
 setClass("MyList", representation(value = "list"))
 
-setGeneric("adding", function(x, ...) {standardGeneric("adding")})
+# setGeric over write the existing generic
 setGeneric("adding", function(x, y, z, ...) {standardGeneric("adding")})
 
-setMethod("adding", signature(x = "MyInteger"), function(x, y) {x@value + y})
-setMethod("adding", signature(x = "MyInteger", y = "numeric", z = "numeric"), function(x, y, z, ....) {x@value + y + z})
+setMethod("adding", signature(x = "MyInteger", y = "numeric"), function(x, y, ...) {x@value + y})
+setMethod("adding", signature(x = "MyInteger", y = "numeric", z = "numeric"), function(x, y, z, ...) {x@value + y + z})
 setMethod("adding", "MyList", function(x, y) {list(x@value, y)})
 
 a <- new("MyInteger", value = 11)
